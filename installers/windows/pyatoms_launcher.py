14a84da165a65935288af05a3b7d04ff032420f7
Jacob Gonzalez <jg.cobi12@gmail.com>
2026-09-02 17:21:47 -0700

    added Windows standalone build files

---
+++ installers/windows/pyatoms_launcher.py
@@ -0,0 +1,15 @@
+"""
+PyAtoms Standalone Launcher
+Created on Wed Sep 02 16:54:20 PM
+@author: Jacob
+
+Modification Log
+----------------
+2026-09-02 - Jacob Gonzalez
+    - Added launcher entry point for standalone PyInstaller builds.
+"""
+
+from pyatoms.PyAtoms_GUI import main
+
+if __name__ == "__main__":
+    main()
