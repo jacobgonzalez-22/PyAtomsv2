9884d2988eea0a0e1486ccab409cfccdcc2d7d31
Jacob Gonzalez <jg.cobi12@gmail.com>
2026-09-03 19:00:26 -0700

    Revert "fixed Windows standalone build"
    This reverts commit 2519f19cc497ec7b026d0c9145d093860b5e3a55.

--- installers/windows/PyAtoms.spec
+++ installers/windows/PyAtoms.spec
@@ -36,7 +36,7 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['pyatoms.ico'],
+    icon=['M:/MemberFiles/Jacob/PyAtoms-main/installers/windows/pyatoms.ico'],
 )
 coll = COLLECT(
     exe,
--- installers/windows/pyatoms_launcher.py
+++ installers/windows/pyatoms_launcher.py
@@ -5,26 +5,9 @@ Created on Wed Sep 02 16:54:20 PM
 
 Modification Log
 ----------------
-2026-09-03 - Jacob Gonzalez
-    - Added null stdout/stderr handling for pyinstaller windowed builds
-
 2026-09-02 - Jacob Gonzalez
-    - Added launcher entry point for standalone PyInstaller builds
+    - Added launcher entry point for standalone PyInstaller builds.
 """
-
-import os
-import sys
-
-# PyInstaller windowed builds do not provide stdout/stderr.
-
-# so redirect them to the null device so libraries that attempt
-# console output do not crash the standalone application
-
-if sys.stdout is None:
-    sys.stdout = open(os.devnull, "w")
-
-if sys.stderr is None:
-    sys.stderr = open(os.devnull, "w")
 
 from pyatoms.PyAtoms_GUI import main
 
